8dade37243a171c324c7640053fc922a0d657f8e
nicolas <nicolas@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-08-24 09:25:32 -0500

    sfPropelPlanetPlugin: fixed syntax error in task class
    git-svn-id: http://svn.symfony-project.com/plugins/sfPropelPlanetPlugin/trunk@11084 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/task/sfPlanetFeedGrabTask.class.php
+++ lib/task/sfPlanetFeedGrabTask.class.php
@@ -5,7 +5,7 @@
  */
 class sfPlanetFeedGrabTask extends sfPlanetBaseTask
 {
-  
+
   /**
    * Task config
    *
@@ -17,7 +17,7 @@ class sfPlanetFeedGrabTask extends sfPlanetBaseTask
     $this->name             = 'feed-grab';
     $this->briefDescription = 'Grabs last planet feeds entries and store them in the database';
     $this->detailedDescription = <<<EOF
-The [planet:feed-grab|INFO] updates outdated feeds, ones from which the 
+The [planet:feed-grab|INFO] updates outdated feeds, ones from which the
 [periodicity|COMMENT] time has been exceeded.
 
 If you want to force grabbing for all feeds, even up-to-date ones:
@@ -28,17 +28,18 @@ If you want verbose logs when fetching feed entries:
 
   $ php symfony planet:feed-grab [-v|COMMENT]
 EOF;
-    
+
     $this->addArguments(array(
       new sfCommandArgument('feed-slug', sfCommandArgument::OPTIONAL, 'The feed slug (if not provided, all active feeds entries will be grabbed)'),
       new sfCommandArgument('application', sfCommandArgument::OPTIONAL, 'The application name', self::getDefaultAppName()),
     ));
-    
+
     $this->addOptions(array(
       new sfCommandOption('force-refresh', 'f', sfCommandOption::PARAMETER_NONE, 'Forces to grab and update all feeds, including those which are not perempted'),
-      new sfCommandOption('verbose', 'v', sfCommandOption::PARAMETER_NONE, 'Prints verbose messages during grabbing'),      
+      new sfCommandOption('verbose', 'v', sfCommandOption::PARAMETER_NONE, 'Prints verbose messages during grabbing'),
     ));
-  
+  }
+
   /**
    * Task execution
    *
@@ -49,18 +50,18 @@ EOF;
   {
     $configuration = ProjectConfiguration::getApplicationConfiguration($arguments['application'], 'cli', true);
     $databaseManager = new sfDatabaseManager($configuration);
-    
+
     $c = new Criteria();
     $c->add(sfPlanetFeedPeer::IS_ACTIVE, true);
     $n = 0;
-    
+
     if ($slug = $arguments['feed-slug'])
     {
       if (is_null($feed = sfPlanetFeedPeer::retrieveBySlug($slug)))
       {
         throw new sfCommandException(sprintf('Feed "%s" does not exist', $slug));
       }
-      
+
       if ($feed->isOutdated() || $options['force-refresh'])
       {
         $this->grabFeedEntries($feed, $options['verbose']);
@@ -69,10 +70,10 @@ EOF;
       {
         $this->logSection('results', 'feed is up to date');
       }
-      
+
       return;
     }
-    
+
     if ($options['force-refresh'])
     {
       $feeds = sfPlanetFeedPeer::doSelect($c);
@@ -81,14 +82,14 @@ EOF;
     {
       $feeds = sfPlanetFeedPeer::getOutdated($c);
     }
-    
+
     foreach ($feeds as $feed)
     {
       $this->grabFeedEntries($feed, $options['verbose']);
       $n++;
     }
-    
+
     $this->logSection('result', ($n > 1 ? $n.' feeds' : ($n > 0 ? 'one feed' : 'no feed')) . ' fetched');
   }
-  
+
 }
